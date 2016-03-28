a75e1651c0e46ef623105b88f9672121498f4ce8
DeltaF1 <delyaerricson@gmail.com>
2016-03-28 19:38:33 -0400

    Added example world

---
+++ world/rooms/misc.lua
@@ -0,0 +1,23 @@
+-- misc.lua
+
+arcane_alcove = {
+  players = {
+  },
+  identifier = "arcane_alcove",
+  name = "Arcane Alcove",
+  exits = {
+    east = "starting",
+  },
+  objects = {
+    [1] = {
+      desc = "A tallow candle, burnt almost to the ground, the flame guttering in a pool of wax.",
+      name = "candle",
+      aliases = {
+      },
+    },
+  },
+  filename = "misc.lua",
+  desc = "Set aside from the other rooms, this space is dimly lit, save for a small flickering candle. The stonework seems to dance in the candelight, and strange runes fade in and out of view. To the east is the starting zone.",
+}
+
+--[[END OF FILE]]--
---
+++ world/rooms/waterraft.lua
@@ -0,0 +1,17 @@
+-- waterraft.lua
+
+tavern_ground_floor = {
+  players = {
+  },
+  identifier = "tavern_ground_floor",
+  name = "Silver Rose - Ground Floor",
+  exits = {
+    down = "starting",
+  },
+  objects = {
+  },
+  filename = "waterraft.lua",
+  desc = "Despite the rough floorboards and meager accomodations, the Silver Rose attracts a modest crowd. ",
+}
+
+--[[END OF FILE]]--

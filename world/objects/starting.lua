6b8c994d262e7b6592eb1cab92a0250991bbc1aa
DeltaF1 <delyaerricson@gmail.com>
2016-03-30 15:06:41 -0400

    Added world structure

---
+++ world/objects/starting.lua
@@ -0,0 +1,21 @@
+-- starting.lua
+
+starting_sign = {
+  identifier = "starting_sign",
+  name = "sign",
+  desc = "The sign reads: Welcome to the server! an exits command will be added, but for now you can go north, west, or up",
+  filename = "starting.lua",
+  aliases = {
+  },
+}
+
+arcane_candle = {
+  identifier = "arcane_candle",
+  name = "candle",
+  aliases = {
+  },
+  filename = "starting.lua",
+  desc = "A tallow candle, burnt almost to the ground, the flame guttering in a pool of wax.",
+}
+
+--[[END OF FILE]]--

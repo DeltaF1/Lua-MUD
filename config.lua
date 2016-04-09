555ff747dc8ecdbea8093dff156f51f710b39690
DeltaF1 <delyaerricson@gmail.com>
2016-04-09 17:35:32 -0400

    Removed salt from config.lua, will be per-account

--- config.lua
+++ config.lua
@@ -3,9 +3,6 @@ return {
 	--
 	-- Miscellaneous options for the MUD server
 	
-	-- If this is changed, users file will be useless
-	salt = "",
-	
 	-- The port that the server binds to. If this port is taken, the server may bind to another available port, check the console output for the bound port
 	port = 5555,
 	motd = "%{green}Welcome to the text-chat-test-server-o-matic-9000"

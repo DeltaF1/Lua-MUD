87c1202edbb97ff9e02361efedc178ca3ae51baa
DeltaF1 <delyaerricson@gmail.com>
2017-12-29 17:19:18 -0500

    Added example config

---
+++ config.lua
@@ -0,0 +1,16 @@
+return {
+	port = 5555,
+	motd = [[%{red}
+THIS IS THE MESSAGE THAT WILL BE SENT AS THE FIRST TEXT BEFORE LOGGING IN.
+%{reset}%{blue}
+THE COLOUR CAN BE MANIPULATED THROUGH COLOUR CODES.
+]],
+	
+	sql_driver = "SQL Driver Name here",
+	sql_db = "lua_mud",
+	
+	sql_user = "root",
+	sql_host = "127.0.0.1",
+	sql_port = 1234,
+	sql_pass = "password",
+}

1f89efe85f2285658e55836e982ed2cc5ccd93ce
DeltaF1 <delyaerricson@gmail.com>
2018-10-02 00:13:36 -0400

    Made schema.sql sqlite compatible

--- schema.sql
+++ schema.sql
@@ -1,132 +1,51 @@
--- MySQL dump 10.16  Distrib 10.1.23-MariaDB, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: lua_mud
--- ------------------------------------------------------
--- Server version	10.1.23-MariaDB-9+deb9u1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `characters`
---
-
-DROP TABLE IF EXISTS `characters`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+PRAGMA synchronous = OFF;
+PRAGMA journal_mode = MEMORY;
+BEGIN TRANSACTION;
 CREATE TABLE `characters` (
-  `identifier` int(11) NOT NULL AUTO_INCREMENT,
-  `user` varchar(30) NOT NULL,
-  `name` varchar(30) DEFAULT NULL,
-  `state` int(11) DEFAULT NULL,
-  `room` int(11) DEFAULT NULL,
-  `description` mediumtext,
-  `colour` int(11) DEFAULT NULL,
-  `cmdset` int(11) DEFAULT NULL,
-  `pronouns` int(11) DEFAULT NULL,
-  `hp` int(11) DEFAULT NULL,
-  PRIMARY KEY (`identifier`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `objects`
---
-
-DROP TABLE IF EXISTS `objects`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+  `identifier` integer NOT NULL PRIMARY KEY AUTOINCREMENT
+,  `user` varchar(30) NOT NULL
+,  `name` varchar(30) DEFAULT NULL
+,  `state` integer DEFAULT NULL
+,  `room` integer DEFAULT NULL
+,  `description` mediumtext
+,  `colour` integer DEFAULT NULL
+,  `cmdset` integer DEFAULT NULL
+,  `pronouns` integer DEFAULT NULL
+,  `hp` integer DEFAULT NULL
+);
 CREATE TABLE `objects` (
-  `identifier` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
-  `description` mediumtext NOT NULL,
-  `container` int(11) NOT NULL,
-  `container_t` tinyint(4) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`identifier`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `pronouns`
---
-
-DROP TABLE IF EXISTS `pronouns`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+  `identifier` integer NOT NULL PRIMARY KEY AUTOINCREMENT
+,  `name` varchar(30) NOT NULL
+,  `description` mediumtext NOT NULL
+,  `container` integer NOT NULL
+,  `container_t` integer NOT NULL DEFAULT '0'
+);
 CREATE TABLE `pronouns` (
-  `identifier` int(11) NOT NULL AUTO_INCREMENT,
-  `i` varchar(10) DEFAULT NULL,
-  `myself` varchar(10) DEFAULT NULL,
-  `mine` varchar(10) DEFAULT NULL,
-  `my` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`identifier`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `rooms`
---
-
-DROP TABLE IF EXISTS `rooms`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+  `identifier` integer NOT NULL PRIMARY KEY AUTOINCREMENT
+,  `i` varchar(10) DEFAULT NULL
+,  `myself` varchar(10) DEFAULT NULL
+,  `mine` varchar(10) DEFAULT NULL
+,  `my` varchar(10) DEFAULT NULL
+);
 CREATE TABLE `rooms` (
-  `identifier` int(11) NOT NULL AUTO_INCREMENT,
-  `description` mediumtext NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `flags` int(11) NOT NULL DEFAULT '0',
-  `exits` varchar(150) NOT NULL,
-  PRIMARY KEY (`identifier`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `user_scripts`
---
-
-DROP TABLE IF EXISTS `user_scripts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+  `identifier` integer NOT NULL PRIMARY KEY AUTOINCREMENT
+,  `description` mediumtext NOT NULL
+,  `name` varchar(100) NOT NULL
+,  `flags` integer NOT NULL DEFAULT '0'
+,  `exits` varchar(150) NOT NULL
+);
 CREATE TABLE `user_scripts` (
-  `identifier` int(11) NOT NULL,
-  `type` tinyint(4) NOT NULL DEFAULT '0',
-  `name` varchar(50) NOT NULL,
-  `body` mediumtext NOT NULL,
-  UNIQUE KEY `uq_user_scripts` (`identifier`,`type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+  `identifier` integer NOT NULL
+,  `type` integer NOT NULL DEFAULT '0'
+,  `name` varchar(50) NOT NULL
+,  `body` mediumtext NOT NULL
+,  UNIQUE (`identifier`,`type`)
+);
 CREATE TABLE `users` (
-  `username` varchar(20) DEFAULT NULL,
-  `password` varchar(32) DEFAULT NULL,
-  `email` varchar(64) NOT NULL,
-  `salt` varbinary(64) NOT NULL,
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-12-28 13:14:12
+  `username` varchar(20) DEFAULT NULL
+,  `password` varchar(32) DEFAULT NULL
+,  `email` varchar(64) NOT NULL
+,  `salt` varbinary(64) NOT NULL
+,  UNIQUE (`username`)
+);
+END TRANSACTION;

b8d541ae0489d0668b1a6164f77d5f8e997b9865
DeltaF1 <delyaerricson@gmail.com>
2020-06-18 01:40:59 -0400

    Add admin account creation to setup.lua

--- setup.lua
+++ setup.lua
@@ -27,12 +27,31 @@ config(defaults.server_info, {port="Server port for clients to connect to", motd
 config(defaults.db_info, {})
 config(defaults.world_info, {})
 
+-- create missing dirs
+-- TODO: add windows-compatible version of this
+os.execute("mkdir " .. defaults.db_info.dataDir)
+os.execute("mkdir " .. defaults.db_info.backupDataDir)
+
+-- load in some helper files
 require "utils"
 NEWL = "\r\n"
 ser = require("world_save").ser
+
+-- Setup Admin
+db = require("filesystem_store")(defaults.db_info.dataDir)
+
+io.write("Admin Username: ")
+adminname = io.read("*l")
+io.write("Admin password: ")
+adminpass = io.read("*l")
+
+db.add_user(adminname, adminpass)
+
+defaults.admin_name = adminname
 
 -- write to config.lua
 local f = io.open("config.lua", "w")
 print(ser(defaults))
 f:write("return "..ser(defaults))
 f:close()
+
